4cb26547660a0089271be680a5a3d3276f0a102c
Felipe Henrique <felipehenrique@ioasys.com.br>
2022-04-08 13:49:42 -0300

    feat: api route removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,12 +45,48 @@ export default function Home({ heroes = []}: HomeProps) {
 
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch("http://localhost:3000/api/heroes");
-  const data = await response.json();
+  const heroes = [
+    {
+        id: 1,
+        actionName: "Learn more",
+        actionLink: "#",
+        title: "AX-1 mission",
+        alt: "Spacex rocket", 
+        subtitle:"upcoming launch",
+        imageUrl: "/images/spacex-hero1.jpg"
+    },
+    {
+        id: 2,
+        actionLink: "#",
+        actionName: "Learn more",
+        title: "AX-1 mission",
+        subtitle: "recent launch",
+        alt: "SpaceX rocket" ,
+        imageUrl: "/images/spacex-hero2.jpg",
+    },
+    {
+        id: 3,
+        actionLink:"#",
+        actionName:"Learn more",
+        title: "Starlink mission",
+        subtitle: "recent launch",
+        alt: "SpaceX starlink mission", 
+        imageUrl: "/images/spacex-hero3.jpg",
+    },
+    {
+      id: 4,
+      actionLink: "#",
+      actionName: "Learn more",
+      title: "STARSHIP TO LAND NASA ASTRONAUTS ON THE MOON",
+      alt: "SpaceX starlink mission", 
+      imageUrl: "/images/spacex-hero4.jpg"
+    }
+]
+
 
   return {
     props: {
-      heroes: data,
+      heroes,
     },
   }
 }
